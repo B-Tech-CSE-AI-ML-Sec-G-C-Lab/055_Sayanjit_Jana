023479bf59e9fed79f381fdff999d3fe4510c347
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-08-07 23:00:37 +0530

    Add Fibonacci series generator in Q5.c

---
+++ Assignment 1 (04.08.26)/Q5.c
@@ -0,0 +1,22 @@
+// * Generate Fibonacci series using Function
+
+#include<stdio.h>
+int fib(int n)
+{
+    if(n<=1)
+    return n;
+    return fib(n-1)+fib(n-2);
+}
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Fibonacci series up to %d terms:\n", n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",fib(i));
+    }
+    return 0;
+}
