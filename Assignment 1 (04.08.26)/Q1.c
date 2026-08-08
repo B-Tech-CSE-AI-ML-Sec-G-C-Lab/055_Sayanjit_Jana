7ba546b435d71949ac21c9c5385c4e3b3378a83e
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-08-08 18:20:57 +0530

    Refactor prime checking program for single input
    Modified the program to check if a single positive number is prime instead of a range.

--- Assignment 1 (04.08.26)/Q1.c
+++ Assignment 1 (04.08.26)/Q1.c
@@ -1,24 +1,22 @@
 //* WAP in C to check prime no using Functions & Loop.
 #include<stdio.h>
 
-int isprime(int n){
+int  isprime(int n){
     if(n<=1) return 0;
     for(int i=2;i<=n/2;i++){
         if(n%i==0) return 0;
     }
     return 1;
 }
+
 int main(){
-    int n,m;
-    printf("Enter a number 1: "); 
-    printf("Enter a number 2: ");
-    scanf("%d %d", &n, &m);
-    for(int i = n;i<=m;i++){
-    if(isprime(i)) {
-        printf("%d is a prime number.\n", i);
+    int n;
+    printf("Enter a positive number : ");
+    scanf("%d", &n);
+    if(isprime(n)) {
+        printf("%d is a prime number.\n", n);
     } else {
-        printf("%d is not a prime number.\n", i);
-    }
+        printf("%d is not a prime number.\n", n);
     }
     return 0;
 }
