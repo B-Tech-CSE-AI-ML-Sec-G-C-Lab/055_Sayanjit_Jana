da07890ba717c695c25f47ec3b895e5fde81174a
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-08-11 13:00:46 +0530

    Add string comparison program in Q5.c
    Implement a C program to compare two strings without using string library functions.

---
+++ Assignment 2 (11.08.26)/Q5.c
@@ -0,0 +1,27 @@
+/*
+C Program to Compare two strings without using string library functions
+*/
+#include <stdio.h>
+int compareStrings(char str1[], char str2[]){
+    int i = 0;
+    while(str1[i] != '\0' || str2[i] != '\0')
+    {
+        if(str1[i] != str2[i])
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+int main(){
+    char str1[100], str2[100];
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+    if(compareStrings(str1, str2))
+        printf("Strings are equal.");
+    else
+        printf("Strings are not equal.");
+    return 0;
+}
