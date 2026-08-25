143d66e39f2ec4e022dca7e9421e3a514dfa8ba5
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-08-25 11:54:54 +0530

    Add C++ program to calculate factorial of a number

---
+++ Assignment 4 (25.08.2026)/Q1.cpp
@@ -0,0 +1,29 @@
+// Write in C++ to find the factorial of a number.
+
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial
+int factorial(int n)
+{
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Factorial = " << factorial(n);
+
+    return 0;
+}
