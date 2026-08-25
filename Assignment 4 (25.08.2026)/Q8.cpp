b7fec5e5f9fb4073b0c3612a0a6f49f2a3354209
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-08-25 12:10:28 +0530

    Implement Car class for details input and display
    Created a Car class to accept and display car details.

---
+++ Assignment 4 (25.08.2026)/Q8.cpp
@@ -0,0 +1,47 @@
+// Create a class “Car” and accept the details of a car for its id and price. Display the details of 2 such car objects.
+
+#include <iostream>
+using namespace std;
+
+class Car
+{
+    int id;
+    float price;
+
+public:
+    // Function to accept car details
+    void getData()
+    {
+        cout << "Enter Car ID: ";
+        cin >> id;
+
+        cout << "Enter Car Price: ";
+        cin >> price;
+    }
+
+    // Function to display car details
+    void display()
+    {
+        cout << "Car ID: " << id << endl;
+        cout << "Car Price: " << price << endl;
+    }
+};
+
+int main()
+{
+    Car c1, c2;
+
+    cout << "Enter details of Car 1:" << endl;
+    c1.getData();
+
+    cout << "\nEnter details of Car 2:" << endl;
+    c2.getData();
+
+    cout << "\nDetails of Car 1:" << endl;
+    c1.display();
+
+    cout << "\nDetails of Car 2:" << endl;
+    c2.display();
+
+    return 0;
+}
